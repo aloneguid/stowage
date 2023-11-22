a92c11737a26e52871af30c0631c4dc0e0b69135
IG <aloneguid@outlook.com>
2023-11-22 22:48:24 +0000

    command line parser

--- src/Stowage.Terminal/Program.cs
+++ src/Stowage.Terminal/Program.cs
@@ -1,18 +1,32 @@
 ﻿using System.Globalization;
+using Spectre.Console.Cli;
+using Stowage.Terminal.Commands;
 using Terminal.Gui;
 
 namespace Stowage.Terminal {
     class Program {
-        static async Task Main(string[] args) {
-            IFileStorage fs = Files.Of.ConnectionString(args[0]);
 
-            // system console doesn't work with text editor (TextView)
-            //Application.UseSystemConsole = true;
-            Application.Init();
+        public static IFileStorage? Fs { get; set; }
 
-            Application.Run(new AppTopLevel(fs));
+        static int Main(string[] args) {
 
-            Application.Shutdown();
+            var app = new CommandApp();
+            app.Configure(config =>
+                config.AddCommand<S3Command>("s3")
+                );
+            int code = app.Run(args);
+
+            if(Fs != null) {
+                // system console doesn't work with text editor (TextView)
+                //Application.UseSystemConsole = true;
+                Application.Init();
+
+                Application.Run(new AppTopLevel(Fs));
+
+                Application.Shutdown();
+            }
+
+            return code;
         }
     }
 }
